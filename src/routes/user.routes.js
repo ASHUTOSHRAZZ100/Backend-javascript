89504bf8183949f6248b5eb725a9010014ce99a7
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-08-17 06:33:48 +0530

    add login route and logout route with middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middlewares.js"
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 
 const router = Router()
@@ -8,16 +9,20 @@ const router = Router()
 router.route("/register").post(
     upload.fields([
         {
-            name : 'avatar',
-            maxCount  :1,
+            name: 'avatar',
+            maxCount: 1,
         },
         {
-            name : "coverImage",
-            maxCount : 1,
+            name: "coverImage",
+            maxCount: 1,
         },
     ]),
     registerUser
 )
-// router.route("/login").post(login)
+router.route("/login").post(loginUser)
+
+// secure routes
+
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
